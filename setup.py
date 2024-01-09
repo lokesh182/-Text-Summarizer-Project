ce837b3fefbb8cfceb29cb076ab6e2bc5b29135f
Lokesh Bapte <lokeshbapte.18@gmail.com>
2024-01-09 11:03:45 +0530

    Package Setup Done

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools 
+
+with open("README.md",encoding="utf-8") as f: # encoding="utf-8" is added to avoid the error: UnicodeDecodeError: 'charmap' codec can't decode byte 0x81 in position 777: character maps to <undefined>
+    long_description = f.read() # read the contents of README.md
+
+__version__ = "0.0.0" # version of the package
+REPO_NAME="-Text-Summarizer-Project" # name of the repository
+AUTHOR_USER_NAME = "lokesh182"
+SRC_REPO="textSummarizer"
+AUTHOR_EMAIL = "lokeshbapte.18@gmail.com"
+
+setuptools.setup(
+    name="SRF_REPO",
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A package to summarize text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"htts://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Yracker":f"htts://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir = {"":"src"},
+    packages = setuptools.find_packages(where="src")
+)
